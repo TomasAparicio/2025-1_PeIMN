dce14111e93863dfb2adce785bb7571aae3f6ac6
TomasAparicio <taparicio@unal.edu.co>
2025-04-09 08:55:02 -0500

    while bien hecho

--- 2025_04_09-Juego/JuegoAdivinar.cpp
+++ 2025_04_09-Juego/JuegoAdivinar.cpp
@@ -2,19 +2,17 @@
 
 int main(void){
     const int NUM = 10;
-    int num;
+    int num = -1;
 
-    for (;;){
+    while (num != NUM){
         std::cout << "Adivina un numero entre 1 y 100:\n";
         std::cin >> num;
-        if (num == NUM){
-        std::cout << "Ganaste, el numero era: " <<NUM <<"\n";
-        break;
-        } else if (num > NUM) {
+        
+        if (num > NUM) {
             std::cout << "El numero que escribiste es muy grande, un poco mas pequeño.\n";
         } else {
-            std::cout << "El numero que escribiste es muy chiquito, te falta.\n";
+            std::cout << "El numero que escribiste es muy chiquito, te falta.\n";}
     }
-    }
+    std::cout << "Ganaste, el numero era: " <<NUM <<"\n";
     return 0;
 }

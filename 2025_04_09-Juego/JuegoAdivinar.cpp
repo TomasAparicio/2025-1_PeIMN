0c674c5f05a19f1a25d61990a2ac6a86b85c6ece
TomasAparicio <taparicio@unal.edu.co>
2025-04-09 08:34:13 -0500

    Juego adivinar, ciclo while

--- 2025_04_09-Juego/JuegoAdivinar.cpp
+++ 2025_04_09-Juego/JuegoAdivinar.cpp
@@ -3,15 +3,16 @@
 int main(void){
     const int NUM = 10;
     int num;
-    std::cout << "Adivina un numero entre 1 y 100:\n";
-    std::cin >> num;
-    if (num == NUM){
-    std::cout << "Haz adivinado el numero, este era: " << NUM << "\n";
-    } else if (num > NUM) {
-        std::cout << "El numero que escribiste es muy grande, un poco mas pequeño.\n";
-    } else {
-        std::cout << "El numero que escribiste es muy chiquito, te falta.\n";
-    }
 
+    while (num != NUM){
+        std::cout << "Adivina un numero entre 1 y 100:\n";
+        std::cin >> num;
+        if (num > NUM) {
+            std::cout << "El numero que escribiste es muy grande, un poco mas pequeño.\n";
+        } else {
+            std::cout << "El numero que escribiste es muy chiquito, te falta.\n";
+    }
+    }
+    std::cout << "Ganaste el numero era" <<NUM <<"\n";
     return 0;
 }

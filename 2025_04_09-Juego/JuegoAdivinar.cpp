8b0f2614fa617c0a7cfa23bf03a07ca81d8fea84
TomasAparicio <taparicio@unal.edu.co>
2025-04-09 08:05:25 -0500

    Juego adivinar, ahora compara el numero

--- 2025_04_09-Juego/JuegoAdivinar.cpp
+++ 2025_04_09-Juego/JuegoAdivinar.cpp
@@ -5,6 +5,13 @@ int main(void){
     int num;
     std::cout << "Adivina un numero entre 1 y 100:\n";
     std::cin >> num;
+    if (num == NUM){
+    std::cout << "Haz adivinado el numero, este era: " << NUM << "\n";
+    } else if (num > NUM) {
+        std::cout << "El numero que escribiste es muy grande, un poco mas pequeño.\n";
+    } else {
+        std::cout << "El numero que escribiste es muy chiquito, te falta.\n";
+    }
 
     return 0;
 }

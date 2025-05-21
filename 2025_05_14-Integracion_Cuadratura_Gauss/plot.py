1637297463e49bbbc376e9fcc11aba3d3b97828d
TomasAparicio <taparicio@unal.edu.co>
2025-05-21 08:45:43 -0500

    Vector aplicacion

---
+++ 2025_05_14-Integracion_Cuadratura_Gauss/plot.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# read data
+N, Err_Trapecio, Err_Simpson, Err_r_Trapecio, Err_r_Simpson  = np.loadtxt("err_vs_N.txt", unpack=True)
+
+# plot 
+fig, ax = plt.subplots()
+ax.plot(N, Err_Trapecio, '-v', label = 'Trapecio')
+ax.plot(N, Err_Simpson, '-^', label = 'Simpson')
+ax.plot(N, Err_r_Trapecio, '-<', label = 'Richardson-Trapecio')
+ax.plot(N, Err_r_Simpson, '->', label = 'Richardson-Simpson')
+
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.set_xlabel('Numero de iteraciones')
+ax.set_ylabel('Error relativo')
+ax.legend()
+fig.savefig('Error_integrales.pdf')
